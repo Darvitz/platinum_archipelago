786b4b5b0c3120115e87b6cccaaf5b73077618d2
Darvitz <vgm.shinshiba@gmail.com>
2025-11-27 16:02:38 -0500

    Change group type from list to string

--- data_gen_templates/items.py
+++ data_gen_templates/items.py
@@ -19,7 +19,7 @@ class ItemData:
     label: str
     id: int
     clas: ItemClass
-    group: [str]
+    group: str
     count: int = 1
     classification: ItemClassification = ItemClassification.filler
 
